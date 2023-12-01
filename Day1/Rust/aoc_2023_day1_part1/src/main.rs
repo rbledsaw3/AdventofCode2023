36bb5964e2bc5dd8cb7d1c68651c0264ecc633ff
Robert Bledsaw III <rbledsaw3@outlook.com>
2023-12-01 12:21:14 -0500

    Rust solution for day1 part1

---
+++ Day1/Rust/aoc_2023_day1_part1/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() -> io::Result<()>{
+    let path = Path::new("inputPart1.txt");
+    let input = File::open(path)?;
+    let buffered = io::BufReader::new(input);
+
+    let mut sum = 0;
+
+    for line in buffered.lines() {
+        let line = line?;
+        let mut first_digit = -1;
+        let mut last_digit = -1;
+
+        for chr in line.chars() {
+            if chr.is_digit(10) {
+                let digit = chr.to_digit(10).unwrap() as i32;
+                if first_digit == -1 {
+                    first_digit = digit;
+                }
+                last_digit = digit;
+            }
+        }
+        if first_digit != -1 && last_digit == -1 {
+            last_digit = first_digit;
+        }
+        sum += first_digit * 10 + last_digit;
+    }
+
+    println!("Total sum: {}", sum);
+    Ok(())
+}
