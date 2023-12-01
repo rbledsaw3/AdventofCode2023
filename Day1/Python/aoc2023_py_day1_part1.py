de6eab70f0fd35936a7b349cea32807423527bec
Robert Bledsaw III <rbledsaw3@outlook.com>
2023-12-01 17:16:12 -0500

    Python solution for day1 part1

---
+++ Day1/Python/aoc2023_py_day1_part1.py
@@ -0,0 +1,23 @@
+def process_line(line):
+    first_digit = -1
+    last_digit = -1
+
+    for char in line:
+        if char.isdigit():
+            digit = int(char)
+            if first_digit == -1:
+                first_digit = digit
+            last_digit = digit
+
+    if first_digit != -1 and last_digit == -1:
+        last_digit = first_digit
+    return first_digit * 10 + last_digit
+
+def process_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    return sum(process_line(line) for line in lines)
+
+total_sum = process_file('inputPart1.txt')
+print(f"Total sum: {total_sum}")
