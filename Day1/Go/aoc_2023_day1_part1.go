3786d2b158f7f005d8af5ac67781bdb4460733bb
Robert Bledsaw III <rbledsaw3@outlook.com>
2023-12-01 16:22:08 -0500

    Go solution written for day 1 part1

---
+++ Day1/Go/aoc_2023_day1_part1.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "unicode"
+)
+
+func main() {
+    file, err := os.Open("inputPart1.txt")
+    if err != nil {
+        fmt.Println("Error reading file", err)
+        return
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    sum := 0
+
+    for scanner.Scan() {
+        line := scanner.Text()
+        firstDigit := -1
+        lastDigit := -1
+
+        for _, chr := range line {
+            if unicode.IsDigit(chr) {
+                digit, _ := strconv.Atoi(string(chr))
+                if firstDigit == -1 {
+                    firstDigit = digit
+                }
+                lastDigit = digit
+            }
+        }
+
+        if firstDigit != -1 && lastDigit == -1 {
+            lastDigit = firstDigit
+        }
+
+        sum += firstDigit*10 + lastDigit
+    }
+
+    if err:= scanner.Err(); err != nil {
+        fmt.Println("Error reading file", err)
+    }
+
+    fmt.Println("Sum:", sum)
+}
---
+++ Day1/Go/go.mod
@@ -0,0 +1,3 @@
+module aoc_2023_day1_part1
+
+go 1.20
