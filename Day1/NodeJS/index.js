2b23e5f64c8ef0717743789ee56d486318536bc0
Robert Bledsaw III <rbledsaw3@outlook.com>
2023-12-01 16:37:29 -0500

    NodeJS solution for day1 part1

---
+++ Day1/NodeJS/index.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const readline = require('readline');
+
+async function processFile() {
+    const fileStream = fs.createReadStream('inputPart1.txt');
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    let sum = 0;
+
+    for await (const line of rl) {
+        let firstDigit = -1;
+        let lastDigit = -1;
+
+        for (const chr of line) {
+            if (!isNaN(chr) && chr.trim() !== '') {
+                const digit = parseInt(chr, 10);
+                if (firstDigit === -1) {
+                    firstDigit = digit;
+                }
+                lastDigit = digit;
+            }
+        }
+        
+        if (firstDigit !== -1 && lastDigit === -1) {
+            lastDigit = firstDigit;
+        }
+
+        sum += firstDigit * 10 + lastDigit;
+    }
+
+    console.log('Total sum: ', sum);
+}
+
+processFile().catch(err => {
+    console.error('Error while processing file', err);
+});
