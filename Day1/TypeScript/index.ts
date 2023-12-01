cd910175654e2f0d2235d2f42be430736cf8c669
Robert Bledsaw III <rbledsaw3@outlook.com>
2023-12-01 16:59:35 -0500

    TypeScript solution day1 part1

---
+++ Day1/TypeScript/index.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs';
+
+function processLine(line: string): number {
+    let firstDigit = -1;
+    let lastDigit = -1;
+
+    for (const chr of line) {
+        if (!isNaN(parseInt(chr)) && chr.trim() !== '') {
+            const digit = parseInt(chr, 10);
+            if (firstDigit === -1) {
+                firstDigit = digit;
+            }
+            lastDigit = digit;
+        }
+    }
+
+    if (firstDigit !== -1 && lastDigit === -1) {
+        lastDigit = firstDigit;
+    }
+
+    return firstDigit * 10 + lastDigit;
+}
+
+function processFile(filePath: string) {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const lines = data.split(/\r?\n/);
+    let sum = 0;
+
+    for (const line of lines) {
+        sum += processLine(line);
+    }
+
+    console.log(`Total sum: ${sum}`);
+}
+
+processFile('inputPart1.txt');
